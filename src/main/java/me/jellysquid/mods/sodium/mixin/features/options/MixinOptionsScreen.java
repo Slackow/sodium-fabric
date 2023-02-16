32cf90865f58d95bbaf7dd65ea5c6311917710d3
andrew <cowslayer6789@gmail.com>
2023-02-16 16:17:28 -0500

    intial tweaks max brightness in the sodium gui is now 500 video settings button now routes to vanilla menu when inside a world

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinOptionsScreen.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinOptionsScreen.java
@@ -2,17 +2,25 @@ package me.jellysquid.mods.sodium.mixin.features.options;
 
 import me.jellysquid.mods.sodium.client.gui.SodiumOptionsGUI;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.VideoOptionsScreen;
 import net.minecraft.client.gui.screen.options.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.options.GameOptions;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Dynamic;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(OptionsScreen.class)
 public class MixinOptionsScreen extends Screen {
+    @Shadow
+    @Final
+    private GameOptions settings;
+
     protected MixinOptionsScreen(Text title) {
         super(title);
     }
@@ -20,7 +28,11 @@ public class MixinOptionsScreen extends Screen {
     @Dynamic
     @Inject(method = "method_19828(Lnet/minecraft/client/gui/widget/ButtonWidget;)V", at = @At("HEAD"), cancellable = true)
     private void open(ButtonWidget widget, CallbackInfo ci) {
-        this.client.openScreen(new SodiumOptionsGUI(this));
+        if (this.client.player != null) {
+            this.client.openScreen(new VideoOptionsScreen(this, this.settings));
+        } else {
+            this.client.openScreen(new SodiumOptionsGUI(this));
+        }
 
         ci.cancel();
     }
